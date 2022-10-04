63481edb4c3245081f140c9438a1bf7dd909641e
su <rakdav@inbox.ru>
2022-10-04 11:23:27 +0200

    Добавьте файлы проекта.

---
+++ CommandPattern/Program.cs
@@ -0,0 +1,58 @@
+﻿Pult pult = new Pult();
+TV tv=new TV();
+pult.SetCommand(new TVOnCommand(tv));
+pult.PressButton();
+pult.PressUndo();
+
+interface ICommand
+{
+    void Execute();
+    void Undo();
+}
+class TV
+{
+    public void On()
+    {
+        Console.WriteLine("Телевизор включен");
+    }
+    public void Off()
+    {
+        Console.WriteLine("Телевизор выключен");
+    }
+}
+class TVOnCommand:ICommand
+{
+    TV tv;
+
+    public TVOnCommand(TV tv)
+    {
+        this.tv = tv;
+    }
+
+    public void Execute()
+    {
+        tv.On();
+    }
+
+    public void Undo()
+    {
+       tv.Off();
+    }
+}
+
+class Pult
+{
+    ICommand command;
+    public void SetCommand(ICommand com)
+    {
+        command = com;
+    }
+    public void PressButton()
+    {
+        command.Execute();
+    }
+    public void PressUndo()
+    {
+        command.Undo();
+    }
+}
---
+++ IteratorPattern/Program.cs
@@ -0,0 +1,79 @@
+﻿
+Library library1 = new Library();
+Reader reader=new Reader();
+reader.SeeBooks(library1);
+
+class Book
+{
+    public string Name { get; set; }
+}
+
+class Reader
+{
+    public void SeeBooks(Library library)
+    {
+        IBookIterator iterator = library.CreateNumerator();
+        while(iterator.HasNext())
+        {
+            Book book = iterator.Next();
+            Console.WriteLine(book.Name);
+        }
+    }
+}
+class Library:IBookNumerable
+{
+    private Book[] books;
+    public Library()
+    {
+        this.books =new Book[]
+        {
+            new Book{Name="Война и мир"},
+            new Book{Name="Отцы и дети"},
+            new Book{Name="Вишневый сад"}
+        };
+    }
+    public Book this[int index]
+    {
+        get { return books[index]; }
+    }
+    public int Count
+    {
+        get { return books.Length; }
+    }
+
+    public IBookIterator CreateNumerator()
+    {
+        return new LibraryNumerator(this);
+    }
+}
+class LibraryNumerator:IBookIterator
+{
+    IBookNumerable aggregate;
+    int index = 0;
+
+    public LibraryNumerator(IBookNumerable a)
+    {
+        this.aggregate = a;
+    }
+
+    public bool HasNext()
+    {
+        return index<aggregate.Count;
+    }
+
+    public Book Next()
+    {
+        return aggregate[index++];
+    }
+}
+interface IBookIterator
+{
+    bool HasNext();
+    Book Next();
+}
+interface IBookNumerable
+{
+    IBookIterator CreateNumerator();
+    int Count { get; }
+    Book this[int index] { get; }
+}
